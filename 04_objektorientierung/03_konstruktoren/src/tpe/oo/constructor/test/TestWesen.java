0375f143949fb95f7d6633fd9239076389ab7df8
Christian Coenen <coenen.christian@outlook.com>
2016-10-24 11:34:18 +0200

    implemented constructors (passed all tests)

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/test/TestWesen.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/test/TestWesen.java
@@ -1,6 +1,12 @@
 package tpe.oo.constructor.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+import tpe.oo.constructor.Nachtelf;
+import tpe.oo.constructor.Untoter;
+import tpe.oo.constructor.Wesen;
 
 /*
 import tpe.constructor.Wesen;
@@ -20,24 +26,20 @@ public class TestWesen {
      */
     @Test
     public void testNachtelf() {
-        /*
         assertTrue("Nachtelf erbt nicht von Figur", new Nachtelf("") instanceof Wesen);
         assertEquals(Nachtelf.STANDARD_NATURWIDERSTAND, new Nachtelf("").getNaturwiderstand());
         assertEquals(120, new Nachtelf("", 120).getNaturwiderstand());
         assertEquals("Hugo", new Nachtelf("Hugo").getName());
-        */
     }
-    
+
     /**
      * Testet den Untoten.
      */
     @Test
     public void testUntoten() {
-        /*
         assertTrue("Untoter erbt nicht von Figur", new Untoter("") instanceof Wesen);
         assertEquals(Untoter.STANDARD_UNTERWASSERATMUNG, new Untoter("").getUnterwasseratmung());
         assertEquals(120, new Untoter("", 120).getUnterwasseratmung());
         assertEquals("Hugo", new Untoter("Hugo").getName());
-        */
     }
 }
