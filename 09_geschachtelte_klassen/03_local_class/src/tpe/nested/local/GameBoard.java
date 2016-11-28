e02cb30298b1bfe9dc9fcd96b22e7c3080d29ce1
Christian Coenen <coenen.christian@outlook.com>
2016-11-28 15:43:43 +0100

    solved 09_01 - 09_03

--- 09_geschachtelte_klassen/03_local_class/src/tpe/nested/local/GameBoard.java
+++ 09_geschachtelte_klassen/03_local_class/src/tpe/nested/local/GameBoard.java
@@ -4,7 +4,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Random;
+
 
 import de.smits_net.games.framework.board.Board;
 
@@ -22,9 +25,19 @@ public class GameBoard extends Board {
     public GameBoard() {
         // neues Spielfeld anlegen
         super(10, new Dimension(800, 300), Color.BLACK);
-
         // Alien initialisieren
         alien = new Alien(this, new Point(800, 50 + new Random().nextInt(100)));
+
+        class AlienExploder extends MouseAdapter {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if(alien.intersects(new Point(e.getX(),e.getY()))) {
+                   alien.explode();
+                }
+            }
+        }
+        AlienExploder alienExploder = new AlienExploder();
+        addMouseListener(alienExploder);
     }
 
     /**
